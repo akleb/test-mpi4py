6b0ed01c5d05b03aa87f0060cbc18c12a5bed154
Alex Kleb <akleb@umich.edu>
2021-10-06 11:41:05 -0400

    Updated the python script

--- python_script.py
+++ python_script.py
@@ -4,6 +4,7 @@ from mpi4py import MPI
 
 print("Trying with MPI_Init()")
 print(subprocess.run("mpirun -np 2 ./main 1", shell=True))
+MPI.COMM_WORLD.Spawn("./main", args=["1"], maxprocs=2)
 
 print("Trying without MPI_Init()")
 print(subprocess.run("mpirun -np 2 ./main 0", shell=True))
